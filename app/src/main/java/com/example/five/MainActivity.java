6c995259459dff091858fd463a4407b33f26711f
Margaret <maenunezt@gmail.com>
2023-06-20 16:19:44 -0400

    This is the initial commit. i just created the design (layout)

--- app/src/main/java/com/example/five/MainActivity.java
+++ app/src/main/java/com/example/five/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.five;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initListeners();
+    }
+
+    private void initListeners() {
+        Button showToast = findViewById(R.id.buttonShowToast);
+        Button counter = findViewById(R.id.buttonCounter);
+        TextView elTexto= findViewById(R.id.elTexto);
+        showToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "hello Toast", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
